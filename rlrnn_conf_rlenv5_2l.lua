b09d28915675fa7a11f6f2f737bf5674e79e16fc
Pengcheng Wang <pwang8@ncsu.edu>
2016-12-28 15:15:08 -0500

    Offer a 2-layer rnn setting file.

---
+++ rlrnn_conf_rlenv5_2l.lua
@@ -0,0 +1,11 @@
+--
+-- User: pwang8
+-- Date: 12/28/16
+-- Time: 3:12 PM
+-- This script offers description of RNN structure configuration
+--
+
+return function(rnnConf)
+    table.insert(rnnConf, 640)
+    table.insert(rnnConf, 320)
+end
